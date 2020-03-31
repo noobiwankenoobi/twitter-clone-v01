b95ca0158307d949734de2263ceed9b6fd938750
Ross Hazelett <rosshazelett@Rosss-MacBook-Pro-2.local>
2020-03-31 13:12:24 -0400

    front end mostly done for now

---
+++ client/app.js
@@ -0,0 +1,19 @@
+const form = document.querySelector('form');
+
+// Event Listener for Form Submission
+form.addEventListener('submit', (event) => {
+    console.log('Form submission working')
+    event.preventDefault()
+    // FormData is built in. Pass in reference to the form
+    const formData = new FormData (form);
+    const name = formData.get('name');
+    const content = formData.get('content')
+    // woof is an object with name a content being inserted
+    const woof = { name, content};
+
+    // Console Logs
+    console.log("dog name =", name)
+    console.log("content =", content)
+    console.log(woof)
+})
+

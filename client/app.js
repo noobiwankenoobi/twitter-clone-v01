b8d0503a91d765d8cd9ebfc7dcdbda4e5aa97fee
Ross Hazelett <rosshazelett@Rosss-MacBook-Pro-2.local>
2020-03-31 16:51:28 -0400

    refactored back end a bit, changed API_URL

--- client/app.js
+++ client/app.js
@@ -1,19 +1,25 @@
-const form = document.querySelector('form');
+const woofForm = document.querySelector('#woof-form');
+const API_URL = 'http://localhost:5000/'
 
 // Event Listener for Form Submission
-form.addEventListener('submit', (event) => {
+woofForm.addEventListener('submit', (event) => {
     console.log('Form submission working')
     event.preventDefault()
     // FormData is built in. Pass in reference to the form
-    const formData = new FormData (form);
-    const name = formData.get('name');
-    const content = formData.get('content')
+    const woofFormData = new FormData (woofForm);
+    const name = woofFormData.get('name');
+    const content = woofFormData.get('content');
     // woof is an object with name a content being inserted
     const woof = { name, content };
 
+    // Send post request to API
+    fetch(API_URL + 'woofs', {
+        method: 'POST',
+        body: JSON.stringify(woof),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
     // Console Logs
-    console.log("dog name =", name)
-    console.log("content =", content)
-    console.log(woof)
+    console.log("woof being sent by client is =", woof)
 })
-
--- server/index.js
+++ server/index.js
@@ -1,21 +1,54 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
-/////////////
-// ROUTES //
-///////////
+// Any server request passing through automatically passes through this middleware and gets correct headers
+app.use(cors())
 
-// Defining what happens on a GET request (request, response)
+// Middleware built into express, any incoming content with content type application/json will be parsed by this
+app.use(express.json())
+
+///////////////////
+// ROUTES ////////
+/////////////////
+
+//////////
+// GET // 
+//////// Defining what happens on a GET request (request, response)
 app.get('/', (req, res) => {
     res.json({
         message: 'Woofer!'
     })
-
 });
 
-// Defining what happens on a POST request
-app.post('/woofs', )
+// Checks to make sure woof content is valid
+function isProperWoof(woof) {
+    // returns a boolean of true if the posted woof object contains a name and content and the name/content aren't empty strings 
+    return woof.name && woof.name.toString().trim() !== '' &&
+           woof.content && woof.content.toString().trim() !== '';
+}
+
+function postWoofToDB(woof) {
+    console.log("woof on backend is =", woof)
+}
+
+///////////
+// POST // 
+///////// Defining what happens on a POST request
+app.post('/woofs', (req, res) => {
+    // putting post request into woof object 
+    const woof = req.body
+    if (isProperWoof(req.body)) {
+        // Put in the database
+        postWoofToDB(woof);
+    } else {
+        res.status(422);
+        res.json({
+            message: 'Name and Content Required'
+        })
+    }
+})
 
 
 // Listening on a certain port
